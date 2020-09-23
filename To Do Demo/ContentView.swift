db4d7d0594ccb831d23f0eb0a887db614e8c77e0
jpeden <john.peden@mtsu.edu>
2020-09-23 14:45:07 -0500

    Initial Commit

---
+++ To Do Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To Do Demo
+//
+//  Created by John M. Peden on 9/23/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ To Do Demo/To_Do_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_DemoApp.swift
+//  To Do Demo
+//
+//  Created by John M. Peden on 9/23/20.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
