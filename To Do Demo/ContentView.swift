15d388164da3cc4396b7c932aadfd7a63860f460
jpeden <john.peden@mtsu.edu>
2020-09-24 08:53:27 -0500

    adds code

--- To Do Demo/ContentView.swift
+++ To Do Demo/ContentView.swift
@@ -1,21 +1,61 @@
-//
-//  ContentView.swift
-//  To Do Demo
-//
-//  Created by John M. Peden on 9/23/20.
-//
-
 import SwiftUI
 
+struct Task: Identifiable {
+    var id = UUID()
+    var title: String
+}
+
 struct ContentView: View {
+    @State var items: [Task] = []
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(items) { item in
+                    TaskView(text: item.title)
+                }.onDelete(perform: { indexSet in
+                    items.remove(atOffsets: indexSet)
+                })
+            }
+            .listStyle(InsetGroupedListStyle())
+            .navigationTitle("To Do")
+            .navigationBarItems(trailing: Button(action: {
+                items.append(Task(title: ""))
+            }) {
+                Image(systemName: "plus")
+            })
+        }
     }
 }
+
+struct TaskView: View {
+    @State var text: String = ""
+    @State var done: Bool = false
+
+    var body: some View {
+        HStack {
+            Button(action: {
+                done = !done
+            }) {
+                Image(systemName: done ? "circle.fill" : "circle.dashed")
+            }.foregroundColor(Color(UIColor.label))
+
+            TextField("", text: $text)
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(items: [Task(title: "take out trash")])
     }
 }
+
+struct TaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskView(text: "take out trash")
+    }
+}
+
+
